400150e458db3715bde07446a1f0a1ec232548df
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2020-11-29 07:56:59 +0700

    ...

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors)
 // app.use(cors({
 //   origin : "http://localhost:9999",
 //   credentials: true,
