3905ff5554c6a9e7e72eca66fd817207e6d4cc66
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2022-03-02 09:37:04 +0700

    fix RF renderer condition statement checking 2

--- public/js/whitelabelGrid.js
+++ public/js/whitelabelGrid.js
@@ -938,7 +938,7 @@ Ext.onReady(function () {
         width: 130,
         dataIndex: 'referralFunction',
         hidden: true,
-        renderer: (value) => (value === true ? 'RF' : 'None'),
+        renderer: (value) => (value === 'None Referral Function' ? 'None' : 'RF'),
       },
       {
         xtype: 'actioncolumn',
