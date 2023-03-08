7402b91b119d850c1e0545173d32aaae32d6918c
phongbugs <nguyenduyphongtn@gmail.com>
2023-03-08 13:59:42 +0700

    add host local

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
     'User-Agent':
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
   },
-  hostBorderPx1: '',
+  hostBorderPx1: 'http://localhost:8888',
   pKey: '',
   pKeyName:
     '-----BEGIN RSA PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCsr6md5JQfMYdD\x2bgp8GclZBmJg\ndAKTjX/fqT8k2cH8aYkiDCNhP4w\x2b5I\x2bfJNsTZOcK6Id\x2bXoCSy\x2bjz9q6G4BSnsbDd\nHM5sEh/3iHwYZXFEs70/BlMKv7I61ixii2aWEfne3m/u33t6uTL4LKWqmpXc3iuP\nwWXkKptVqkl13fD3GwIDAQAB\n-----END RSA PUBLIC KEY-----\n',
